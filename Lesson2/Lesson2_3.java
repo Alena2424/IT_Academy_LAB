94e48fae392a894e3976dfc213fca528ad5efb73
Elena Shalokha <alena.shaloha@gmail.com>
2022-09-09 22:05:41 +0300

    Delete Lesson2 directory

--- Lesson2/Lesson2_3.java
+++
@@ -1,8 +0,0 @@
-public class Lesson2_3 {
-    public static void main(String ...args){
-        int r = 4;
-        int degree = 2;
-        System.out.println ("Площадь окружности = " + (3.14 *  Math.pow(r, degree)));
-        System.out.println ("Длинна окружности = " + (2 * 3.14 * r));
-    }
-}
