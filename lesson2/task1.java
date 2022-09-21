6697e7c69a413e13ad2fa207d3231e6858f8bbad
Elena Shalokha <alena.shaloha@gmail.com>
2022-09-21 20:41:53 +0300

    Add files via upload

---
+++ lesson2/task1.java
@@ -0,0 +1,10 @@
+package lesson2;
+
+public class task1 {
+        public static void main(String[] args) {
+            int x = 17;
+            int y = 5;
+            System.out.println("Результат от деления = " + (x/y));
+            System.out.println("Остаток от деления = " + (x%y));
+        }
+    }
---
+++ lesson2/task2.java
@@ -0,0 +1,10 @@
+package lesson2;
+
+public class task2 {
+        public static void main(String[] args) {
+            int width = 12;
+            int height = 6;
+            System.out.println("Периметр прямоугольника = " + ((width + height) * 2));
+            System.out.println("Площадь прямоугольника = " + (width * height));
+        }
+    }
